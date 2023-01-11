10acb043f73d6fa0c4204fe6967008429c4c8b7a
vntouch <112453462+vntouch@users.noreply.github.com>
2023-01-11 15:46:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,10 @@ install_3proxy() {
     cd 3proxy-0.9.3
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    cp src/3proxy.rc /usr/local/etc/3proxy/bin
+    cp ./src/3proxy.rc /usr/local/etc/3proxy/bin
+    cp scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    cp ./scripts/init.d/3proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
